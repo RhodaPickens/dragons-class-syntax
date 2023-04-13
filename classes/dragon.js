dc9d5c14bb8b82b6d2858875acd4272f192092d1
Rhoda Pickens <rhodajane@gmail.com>
2023-04-13 14:09:43 -0500

    Finished Phase 1

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,30 @@
-// Your code here
+/* Define a static method to the Dragon class. The method should be named getDragons
+ accept any number of instances of the Dragon class that are passed into it as separate arguments.
+ The getDragons static method should return an array of all the dragon names
+from the instances that were passed into it as arguments.
+*/
+
+// Define dragon class
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  // Define instance method that returns a string
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    let dragonNames = dragons.map(function(dragon) {
+      return dragon.name;
+    })
+    return dragonNames;
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
