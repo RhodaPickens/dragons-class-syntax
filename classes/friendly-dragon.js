545219bb294fafe488b1fc5a1e050c5161c3b3a5
Rhoda Pickens <rhodajane@gmail.com>
2023-04-13 20:16:11 -0500

    Created Friendly Dragon and Methods

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,26 @@
-// Your code here
+const Dragon = require("./dragon");
+
+class FriendlyDragon extends Dragon {
+  constructor(name, color, lifeGoals, friend) {
+    super(name, color);
+    this.lifeGoals = lifeGoals;
+    this.friend = friend;
+  }
+
+  hasLifeGoals() {
+    let dragonName = this.name;
+    this.lifeGoals.forEach(function(lifegoal) {
+      console.log(`${dragonName} likes to ${lifegoal}`);
+    });
+  }
+
+  helpsPeople() {
+    return `${this.name} helps their friend ${this.friend}`;
+  }
+
+}
+
+
+/****************************************************************************/
+
+module.exports = FriendlyDragon;
